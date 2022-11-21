83271a8831cb0e8b3b854434566e38a82e4a0db4
maromaSamsa <y9740201@gmail.com>
2022-11-21 17:00:20 +0800

    Test: detect whether hand is shown

--- AR_hand_detection/ContentView.swift
+++ AR_hand_detection/ContentView.swift
@@ -11,7 +11,7 @@ import Metal
 struct ContentView: View {
     var body: some View {
         let description = AppCoordinator.AppDescription(name: "ARMT APP")
-        ARContentView<EmptySettingsView>().environmentObject(Coordinator(appDescription: description))
+        ARContentView<SettingView>().environmentObject(Coordinator(appDescription: description))
     }
 }
 
@@ -24,8 +24,33 @@ class MTMainRenderer: MainRenderer{
 
 
 class Coordinator: AppCoordinator{
+    var isRecording = false
+    let semaphore = DispatchSemaphore(value: 10)
+    let queue = DispatchQueue.init(label: "Recording hand using time", qos: .background )
     override var makeMainRenderer: AppCoordinator.MainRendererInitializer{
         MTMainRenderer.init
+    }
+    override func modifyCustomSettings(customSettings: inout [String : String]) {
+        queue.async {
+            if self.semaphore.wait(timeout: .distantFuture) == .success {
+                if self.isRecording && !self.settingsAreShown{
+                    print(NSDate(), self.handIsDetected)
+                }
+                sleep(1)
+                self.semaphore.signal()
+            }
+        }
+    }
+}
+
+struct SettingView:CustomRenderingSettingsView{
+    @ObservedObject var coordinator: Coordinator
+    init(c: Coordinator) {
+        self.coordinator = c
+    }
+    
+    var body: some View{
+        Toggle("Start Recording", isOn: $coordinator.isRecording)
     }
 }
 
